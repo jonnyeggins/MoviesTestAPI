8579c3ce7d63c5ad246948cdd00fd940415855cf
Jonny Eggins <iliveontheinternet@hotmail.com>
2017-12-16 06:51:28 +1000

    Added Database Migrations

--- database/migrations/2017_12_13_102849_create_genres_rel_actors_table.php
+++ database/migrations/2017_12_13_102849_create_genres_rel_actors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateGenresRelActorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('genres_rel_actors', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('api_token');
-            $table->rememberToken();
+            $table->integer('actor_id');
+            $table->integer('genre_id');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,25 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('actors')->insert([
+            'name' => 'Hugh Jackman',
+            'birth_date' => '15/15/1980',
+            'age' => '60',
+            'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos soluta ratione iusto quasi, sapiente perferendis, aut qui unde optio, vero nisi. Ullam amet laboriosam quaerat, nihil, cumque consectetur dolore aliquam.',
+            'image' => 'http://asasdadads',
+        ]);
+        DB::table('actors')->insert([
+            'name' => 'Denzel Washington',
+            'birth_date' => '15/15/1980',
+            'age' => '60',
+            'bio' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos soluta ratione iusto quasi, sapiente perferendis, aut qui unde optio, vero nisi. Ullam amet laboriosam quaerat, nihil, cumque consectetur dolore aliquam.',
+            'image' => 'http://asasdadads',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Test API User',
+            'email' => 'mail@jonnyeggins.com',
+            'password' => str_random(60),
+            'api_token' => 'skdq90pokalsm4390weiojskm390weiosdklm29e0wioajslzk'
+        ]);
     }
 }
