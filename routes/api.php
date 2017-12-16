1fed7a433e152465947dd5884e4026dd59762d36
Jonny Eggins <iliveontheinternet@hotmail.com>
2017-12-17 00:43:57 +1000

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -2,17 +2,49 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'v1','middleware' => 'auth:api'],	function(){
+
+
+		//MOVIES
+		Route::get('/movies','MoviesController@index');
+		Route::post('/movies','MoviesController@store');
+		Route::get('/movies/{id}','MoviesController@show');
+		Route::patch('/movies/{id}','MoviesController@update');
+		Route::delete('/movies/{id}','MoviesController@destroy');
+		Route::get('/movies-by-genre/{genre_id}','MoviesController@getMoviesByGenre');
+
+
+		//ACTORS
+		Route::get('/actors','ActorsController@index');
+		Route::post('/actors','ActorsController@store');
+		Route::get('/actors/{id}','ActorsController@show');
+		Route::patch('/actors/{id}','ActorsController@update');
+		Route::delete('/actors/{id}','ActorsController@destroy');
+		Route::get('/actors-by-genre/{genre_id}','ActorsController@getActorsByGenre');
+		
+		//GENRES
+		Route::get('/genres','GenresController@index');
+		Route::post('/genres','GenresController@store');
+		Route::get('/genres/{id}','GenresController@show');
+		Route::patch('/genres/{id}','GenresController@update');
+		Route::delete('/genres/{id}','GenresController@destroy');
+		
+		Route::post('/uploadActorImage',function(){
+			//upload the image to s3
+			//$file = request()->file('avatar');
+			//$file->storeAs('actors/'.auth()->id.'actor.jpg');
+
+		});
+
+		
+});
+
+Route::get('/closure',function () {
+    dd('fail');
 });
